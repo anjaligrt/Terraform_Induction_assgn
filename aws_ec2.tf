c05ae574bc406b504f90068adafde3eef6b07a53
anjaligrt <102939525+anjaligrt@users.noreply.github.com>
2023-02-16 15:11:07 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -23,5 +23,7 @@ resource "aws_instance" "webserver" {
 }
 
 
-# Using Environment Variables
 
+# Using Environment Variables
+$ export AWS_ACCESS_KEY_ID="anaccesskey"
+$ export AWS_SECRET_ACCESS_KEY="asecretkey"
