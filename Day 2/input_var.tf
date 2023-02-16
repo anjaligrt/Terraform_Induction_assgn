97a5192b8e2f2c83f06dbc434617bf29c1522caa
anjaligrt <102939525+anjaligrt@users.noreply.github.com>
2023-02-16 15:37:34 +0530

    Update input_var.tf

--- Day 2/input_var.tf
+++ Day 2/input_var.tf
@@ -3,19 +3,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+     # version = "~> 4.16"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "ap-south-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-0f8ca728008ff5af4"
   instance_type = "t2.micro"
 
   tags = {
