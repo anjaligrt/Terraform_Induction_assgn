23750e767099a03d886b51a67e2c5646f83c5bbe
anjaligrt <102939525+anjaligrt@users.noreply.github.com>
2023-03-01 16:48:02 +0530

    Update vars.tf

--- Rancher node/vars.tf
+++ Rancher node/vars.tf
@@ -6,6 +6,25 @@ variable "owner_id" {
   type = string  
 }
 
+variable "vpc_name" {
+  type = string  
+}
+
+variable "subnet_name" {
+  type = string  
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "vol" {
+  type = list(object({
+    vol_name = string
+    vol_type = string
+  }))  
+}
+
 variable "ami" {
   type = string
 }
