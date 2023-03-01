224f1af3bf4ee08d08804ea972bd7cc3d8cbb131
anjaligrt <102939525+anjaligrt@users.noreply.github.com>
2023-03-01 15:52:48 +0530

    Create vars.tf

---
+++ Rancher node/vars.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "owner_id" {
+  type = string  
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "inst_name" {
+  type = string
+}
